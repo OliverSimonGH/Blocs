9859ac9cc7a691629d3623a6c5e68b8ef29365dc
c1641195 <c1641195>
2016-11-09 03:23:21 +0000

    Made first draft of navbar on navbar.html, added blocs.py for app routes, app route currently unfunctional, template code for navbar on home.html but also not functional yet, added Bulma.io framework.

---
+++ blocs.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, redirect, request,render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/navbar_template", methods=['GET'])
+def navigation():
+    if request.method == 'GET':
+        return render_template('navbar.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
