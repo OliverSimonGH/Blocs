f8ab1ff6a6713503d70915c3ef4f44680a742d56
Alexander Miles <alexandergmiles>
2016-11-17 01:31:24 +0000

    Added ability to write to database simply, need to extract data from form

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, request, render_template, jsonify
+import database
+import sqlite3
 app = Flask(__name__, static_url_path="/static")
 
 #Home section - Adding blocks to database and removing from database
 @app.route("/")
 @app.route("/home")
 def home():
-    #Changed things
+    database.select_all()
     return render_template('index.html')
 
+@app.route("/uploadBloc", methods=['POST'])
+def upload_bloc():
+    parameters = ["Second bloc", "This is the second bloc", "http://www.google.co.uk"]
+    database.write_bloc_to_database(parameters)
+    result = database.select_all()
+    print(result)
+    result = jsonify(result)
+    return result
 #Emails - CRUD emails
 @app.route("/emails")
 def emails():
@@ -31,5 +40,10 @@ def settings():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+    database.delete_tables()
+    database.create_tables()
+    database.populate_tables()
+    database.select_all()
