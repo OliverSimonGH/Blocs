fa09b552060a531732157155c3c42735b1bbb327
Alexander Miles <milesa6@cardiff.ac.uk>
2016-11-29 07:53:44 +0000

    Revert "Merge branch 'CRUD-blocs' into 'development'"
    This reverts merge request !3

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
-
-from flask import Flask, request, render_template, jsonify, redirect, Markup
+from flask import Flask, request, render_template, jsonify, redirect
 import database
 import sqlite3
 import os, re
@@ -23,49 +22,49 @@ def upload_bloc():
 
 @app.route("/sendEmail", methods=['POST'])
 def sendEmail():
-   email_sent = request.form.get('send-email')
-   email_radio = request.form.get('send-radio')
-   check = bool(email_radio)
-   email_val = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+    email_sent = request.form.get('send-email')
+    email_radio = request.form.get('send-radio')
+    check = bool(email_radio)
+    email_val = re.compile(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
 
-   if email_sent == "":
-       msg = "Please enter an e-mail address"
+    if email_sent == "":
+        msg = "Please enter an e-mail address"
 
-   elif not email_val.match(email_sent):
-       msg = "Please enter a valid e-mail address"
+    elif not email_val.match(email_sent):
+        msg = "Please enter a valid e-mail address"
 
-   else:
-       # store data into database
-       conn = sqlite3.connect(DATABASE)
-       cur = conn.cursor()
-       cur.execute("INSERT INTO Emails ('emailAddress', 'emailList')\
-                    VALUES (?,?)", (email_sent, check))
-       conn.commit()
-       msg = "You have sent an e-mail to: " + email_sent
-   return render_template("index.html", msg=msg)
+    else:
+        # store data into database
+        conn = sqlite3.connect(DATABASE)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Emails ('emailAddress', 'emailList')\
+                     VALUES (?,?)", (email_sent, check))
+        conn.commit()
+        msg = "You have sent an e-mail to: " + email_sent
+    return render_template("index.html", msg=msg)
 
 @app.route("/readBloc", methods=['GET'])
 def read_bloc():
-   pass
+    pass
 
 #Emails - CRUD emails
 @app.route("/emails", methods=['GET'])
 def emails():
-   conn = sqlite3.connect(DATABASE)
-   cur = conn.cursor()
-   cur.execute("SELECT emailAddress FROM Emails WHERE emailList = 1")
-   email_address = cur.fetchall()
-   conn.close()
-   return render_template('emails.html',  email_address = email_address)
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    cur.execute("SELECT emailAddress FROM Emails WHERE emailList = 1")
+    email_address = cur.fetchall()
+    conn.close()
+    return render_template('emails.html',  email_address = email_address)
 
 #Logs - Add and view logs
 @app.route("/logs")
 def logs():
-   return render_template('logs.html')
+    return render_template('logs.html')
 
 @app.route("/profile")
 def profile():
-   return render_template('profile.html')
+    return render_template('profile.html')
 
 @app.route("/settings")
 def settings():
@@ -73,7 +72,7 @@ def settings():
 
 @app.errorhandler(404)
 def page_not_found(e):
-   return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 
 @app.route('/')
@@ -113,9 +112,9 @@ def editBlocForm():
     return redirect('/')
 
 if __name__ == "__main__":
-   database.delete_tables()
-   database.create_tags()
-   database.create_tables()
-   database.populate_tables()
-   database.select_all()
-   app.run(debug=True)
+    database.delete_tables()
+    database.create_tags()
+    database.create_tables()
+    database.populate_tables()
+    database.select_all()
+    app.run(debug=True)
