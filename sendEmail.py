40919ddb8ac6d50bcde65b90aac1bda540a3bd7f
Emma Brady <bradye@cardiff.ac.uk>
2016-12-03 23:38:16 +0000

    All appropriate information is pulled from database to be put into email

--- sendEmail.py
+++ sendEmail.py
@@ -7,49 +7,27 @@ from email.mime.text import MIMEText
 DATABASE = 'Blocs.db'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+print("Getting blocs from server...")
 conn = sqlite3.connect(DATABASE)
 cur = conn.cursor()
-test = cur.execute("SELECT weburl FROM Blocs")
-print(test)
-#blocs = cur.fetchall()
-
-#print("Getting blocs from server...")
-# print(blocs)
-# web_url_list = []
-# img_url_list = []
-# for record in blocs:
-#     name = record[1::6]
-#     print("------------------------")
-#     print("Name:")
-#     print(name)
-#     print("------------------------")
-#
-#     img_URL = record[2::6]
-#     print("........................")
-#     print("img_URL")
-#     print(img_URL)
-#     print("........................")
-#
-#     title = record[3::6]
-#     print("________________________")
-#     print("title")
-#     print(title)
-#     print("________________________")
-#
-#     notes = record[4::6]
-#     print("========================")
-#     print("notes")
-#     print(notes)
-#     print("========================")
-
+cur.execute("SELECT weburl FROM Blocs")
+weburl_data = cur.fetchall()
+cur.execute("SELECT imgurl FROM Blocs")
+imgurl_data = cur.fetchall()
+cur.execute("SELECT title FROM Blocs")
+title_data = cur.fetchall()
+cur.execute("SELECT notes FROM Blocs")
+notes_data = cur.fetchall()
 conn.commit()
 conn.close()
 
-# print("Web URL:", name)
-# print(" ")
-# print("Image URL:", img_URL)
-# print("Title:", title)
-# print("Notes:", notes)
+print(weburl_data)
+print("")
+print(imgurl_data)
+print("")
+print(title_data)
+print("")
+print(notes_data)
 
 from_email = "bradye@cardiff.ac.uk"
 from_pwd = "Summertime11"
@@ -61,28 +39,21 @@ msg['Subject'] = "Test SMTPlib Message"
 msg['From'] = "bradye@cardiff.ac.uk"
 msg['To'] = "BlocsTest@outlook.com"
 
-firstHTML = "<html> <head></head> <body><table></table>"
-bloc_one = "<div>This is the bloc title</div>"
-secondHTML = "</body></html>"
-print(firstHTML)
+firstHTML = '<html> <head></head> <body><table style="border: 1px solid black; font-color:red;"> <tr>'
+bloc_one = "weburl_data"
+secondHTML = '</tr></table></body></html>'
 
-# bloc_one = "<div>This is the bloc title</div>"
-# secondHTML = "</body></html>"
-#
-# new_html = firstHTML + bloc_one + secondHTML
-#
-# # firstHTML.append(bloc_one)
-# # firstHTML.append(secondHTML)
-#
-# msg.attach(MIMEText(new_html, 'html'))
-# print(msg)
-#
-# # May want to add some error checks here
-# # Change these based on the SMTP params of your mail provider
-# mail = smtplib.SMTP('outlook.office365.com', 587)
-# mail.ehlo()
-# mail.starttls()
-# mail.login("bradye@cardiff.ac.uk", "Summertime11")
-# mail.sendmail("bradye@cardiff.ac.uk", "BlocsTest@outlook.com", msg.as_string())
-# print("email sent")
-# mail.quit()
+new_html = firstHTML + bloc_one + secondHTML
+
+msg.attach(MIMEText(new_html, 'html'))
+print(msg)
+
+# May want to add some error checks here
+# Change these based on the SMTP params of your mail provider
+mail = smtplib.SMTP('outlook.office365.com', 587)
+mail.ehlo()
+mail.starttls()
+mail.login("bradye@cardiff.ac.uk", "Summertime11")
+mail.sendmail("bradye@cardiff.ac.uk", "BlocsTest@outlook.com", msg.as_string())
+print("email sent")
+mail.quit()
